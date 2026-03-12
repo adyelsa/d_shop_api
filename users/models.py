623b8d68580490d57be1664241027224b5969eb7
adyelsa <adylbekelza@gmail.com>
2026-03-12 13:32:27 -0700

    Добавила поля first_name, last_name и registration_source в модель пользователя. Подготовила модель для OAuth авторизации.

--- users/models.py
+++ users/models.py
@@ -10,9 +10,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(max_length=30, blank=True, null=True)
     birthdate = models.DateField(null=True, blank=True)
 
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+
+    registration_source = models.CharField(max_length=20, default="local")
+
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
 
     objects = UserManager()
 
@@ -27,5 +31,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def generate_code(length: int = 6) -> str:
         return ''.join(random.choices(string.digits, k=length))
-
 
